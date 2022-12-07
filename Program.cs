ea565dd4de7fdfd6508fd4025464f5724f78c4c3
Mikhail <jeveuxmourir31613@gmail.com>
2022-12-07 04:14:03 +0300

    функция вычисления и вывода измененного массива + блок-схема

--- Program.cs
+++ Program.cs
@@ -12,16 +12,33 @@
     return arr;
 }
 
-void writeArr(string[] startArr)
+void writeArr(string[] startArr1)
 {
     Console.Write($"\n");
     Console.WriteLine($"Ваш массив: ");
-    for (int i = 0; i < startArr.Length; i++)
+    for (int i = 0; i < startArr1.Length; i++)
     {
-        Console.Write($"\"{startArr[i]}\"   ");
+        Console.Write($"\"{startArr1[i]}\"   ");
+    }
+}
+
+void newArr(string[] startArr1)
+{
+    string[] someNewArr = new string[startArr1.Length];
+    Console.Write($"\n");
+    Console.WriteLine($"Массив, в котором остались только элементы с длиной не более 3 символов: ");
+    for (int i = 0; i < startArr1.Length; i++)
+    {
+        if (startArr1[i].Length <= 3)
+        {
+            someNewArr[i] = startArr1[i];
+            Console.Write($"\"{someNewArr[i]}\"   ");
+        }
     }
 }
 
 string[] startArr = addArr();
 
 writeArr(startArr);
+
+newArr(startArr);
