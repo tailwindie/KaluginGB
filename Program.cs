c96c25fe37e6e59a627f2d5ace57a3c05a1ff68d
Mikhail <jeveuxmourir31613@gmail.com>
2022-12-07 03:19:03 +0300

    функция создания массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] addArr()
+{
+    Console.Write($"Введите количество элементов в массиве: ");
+    int arrNums = Convert.ToInt32(Console.ReadLine());
+    string[] arr = new string[arrNums];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Введите элемент под номером {i+1}: ");
+        arr[i] = Console.ReadLine();
+    }
+
+    return arr;
+}
+
+string[] arr1 = addArr();
